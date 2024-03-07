d5e94e3d1f518bca334da9e59c041f1ab6ba7497
elijah <elireid.28@gmail.com>
2024-03-07 13:58:22 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MenuItem} from './models/menu-item'
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,64 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'GCLab21';
+  chirpusMenu:MenuItem[] = [
+    {
+      name: "Mom's Spaghetti",
+      category: "pasta",
+      price: 12.99,
+      vegetarian: true,
+      soldOut: true
+    },
+    {
+      name: "Mystery Meat Sandwich",
+      category: "sub",
+      price: 10.99,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Speed Cola",
+      category: "drink ",
+      price: 3.99,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Linguine",
+      category: "pasta",
+      price: 13.99,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "$5 Footlong",
+      category: "sub",
+      price: 13.29,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Butter Beer",
+      category: "drink ",
+      price: 6.99,
+      vegetarian: true,
+      soldOut: true
+    },
+    {
+      name: "Pepperoni Pizza",
+      category: "pizza",
+      price: 10.99,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Margherita Pizza",
+      category: "pizza",
+      price: 10.99,
+      vegetarian: false,
+      soldOut: false
+    }
+  ];
+
+
 }
